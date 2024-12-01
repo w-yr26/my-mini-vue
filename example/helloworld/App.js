75ad81ca0a8b96aefd3d2bf12e84188ea04a83b4
wyr <18026086011@163.com>
2024-12-01 09:44:12 +0800

    feat: init createElement, handle type & props(not style) & children(string array)

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,20 @@ import { h } from '../../lib/mini-vue.es.js'
 export const App = {
   render() {
     // ui
-    return h('div', 'hi' + this.msg)
+    return h(
+      'div',
+      {
+        class: 'container',
+        id: 'app',
+      },
+      // children -> string
+      // 'hi mini-vue'
+      // children -> Array
+      [
+        h('p', { class: 'red' }, 'hello'),
+        h('p', { class: 'green' }, 'mini-vue'),
+      ]
+    )
   },
   setup() {
     return {
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,32 +1,74 @@
+import { isObject } from '../shared/index'
 import { createComponentInstance, setupComponent } from './component'
 
-export function render(vnode, rootContainer) {
-  patch(vnode, rootContainer)
+export function render(vnode, container) {
+  patch(vnode, container)
 }
 
-function patch(vnode, rootContainer) {
+function patch(vnode, container) {
   // 通过type判断是去处理 Component 类型 or element 类型
-  // TODO element
+  // 如果是组件，vnode.type是组件对象
 
-  // 渲染组件类型
-  processComponent(vnode, rootContainer)
+  if (typeof vnode.type === 'string') {
+    // 渲染element类型
+    processElement(vnode, container)
+  } else if (isObject(vnode.type)) {
+    // 渲染组件类型
+    processComponent(vnode, container)
+  }
 }
 
-function processComponent(vnode, rootContainer) {
-  mountComponent(vnode, rootContainer)
+/**
+ * 此时的vnode结构类似于：
+ * vnode = {
+ *  type: 'div',
+ *  props: {
+ *    class: '',
+ *    id: ''
+ *  },
+ *  children: 'string' or [ h(), h(), 'string' ]
+ * }
+ *
+ */
+function processElement(vnode, container) {
+  //  创建节点
+  const el = document.createElement(vnode.type)
+  // children -> string or Array
+  const { children } = vnode
+  if (typeof children === 'string') {
+    // string类型，直接设置内容
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    // Array类型，递归调用patch -> 进入patch后，再度判断是渲染Component or element
+    children.forEach((v) => {
+      patch(v, el)
+    })
+  }
+  // props
+  const { props } = vnode
+  for (const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+
+  container.append(el)
+}
+
+function processComponent(vnode, container) {
+  mountComponent(vnode, container)
 }
 
 // 组件挂载
-function mountComponent(vnode, rootContainer) {
+function mountComponent(vnode, container) {
   const instance = createComponentInstance(vnode)
   // 处理setup部分
   setupComponent(instance)
   // 处理render
-  setupRenderEffect(instance, rootContainer)
+  setupRenderEffect(instance, container)
 }
 
-function setupRenderEffect(instance, rootContainer) {
+function setupRenderEffect(instance, container) {
   // 组件实例的render属性挂载着组件内的render()，而组件内的render()返回一个h()，h()是用来创建虚拟节点的，但此时创建的虚拟节点的type类型就不再是Component，而是element，所以要再次调用render()进行element的渲染
   const subTree = instance.render()
-  patch(subTree, rootContainer)
+  patch(subTree, container)
 }
